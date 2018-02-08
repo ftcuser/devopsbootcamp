4ea10421020bcd8b528635c8cb273542455a268b
ftcuser <rupesh.kumar@citizant.com>
2018-02-08 12:50:20 -0500

    Add files via upload

---
+++ TestSureFire.java
@@ -0,0 +1,30 @@
+package com.citizant.todo;
+
+import org.junit.Test;
+
+public class TestSureFire {
+	
+@Test
+public void testcaseFirst()
+	{
+ System.out.println("First Test case executed");
+  }
+
+@Test
+public void testcaseSecond()
+{
+System.out.println("Second Test case executed");
+}
+	
+@Test
+public void testcaseThird()
+{
+System.out.println("Third Test case executed");
+}
+@Test
+public void testcaseFourth()
+{
+System.out.println("Fourth Test case executed");
+}
+
+}
